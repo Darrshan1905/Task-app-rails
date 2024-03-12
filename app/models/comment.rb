896cd425ff505ea2c4c6501ff99fd57bc1076bb4
Darrshan1905 <darrshan@mobicip.com>
2024-03-12 18:52:33 +0530

    fixed dependent bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :task, dependent: :destroy
+  belongs_to :task
   validates :commenter, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: {minimum: 5}
 end
