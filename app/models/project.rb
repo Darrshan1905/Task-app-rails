fc7222a671ad57dd23267921f5f5d0c1aeb15c7b
Darrshan1905 <darrshan@mobicip.com>
2024-03-21 17:43:26 +0530

    modified the validation layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,10 @@ class Project < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
     validates :start_date, presence: true
     validates :end_date, presence:true
+    validate :start_date_before_end_date
+end
+
+def start_date_before_end_date
+    return unless start_date && end_date
+    errors.add(:end_date, "must be after start date") if end_date < start_date
 end
